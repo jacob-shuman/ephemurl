35d6a4846593f1859ce246c434b3e84ad8fe1c03
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2023-09-01 20:28:53 -0400

    chore: enable card icon

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-  // import * as Icons from "@tabler/icons-svelte";
+  import * as Icons from "@tabler/icons-svelte";
 
-  // export let icon: keyof typeof Icons;
+  export let icon: keyof typeof Icons;
   export let url: string;
 
-  // const Icon: any = Icons[icon];
+  const Icon: any = Icons[icon];
 </script>
 
 <a href={url} class="flex flex-col gap-y-2 group">
   <div
     class="bg-tinge dark:bg-bauhaus w-64 h-32 rounded-lg group-hover:-translate-y-2 scale transition-all duration-100 ease-out group-hover:bg-bauhaus group-hover:dark:bg-tinge flex justify-center items-center"
   >
-    <!-- <svelte:component
+    <svelte:component
       this={Icon}
       class="w-12 h-12 text-grey group-hover:text-tinge group-hover:dark:text-bauhaus"
-    /> -->
+    />
   </div>
 
   <div class="flex flex-col gap-y-1">
