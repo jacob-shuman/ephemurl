e3393a92c972df11aba0a2ef022bec33bb79d0bf
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2023-09-01 20:24:10 -0400

    chore: temporarily disable tabler icons

--- src/components/nav-button.svelte
+++ src/components/nav-button.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { IconMoonFilled, IconSunFilled } from "@tabler/icons-svelte";
+  // import { IconMoonFilled, IconSunFilled } from "@tabler/icons-svelte";
 </script>
 
 <button
   class="w-12 h-12 flex justify-center items-center rounded-lg text-grey bg-tinge dark:bg-bauhaus"
 >
-  <IconSunFilled class="w-6 h-6 dark:hidden" />
-  <IconMoonFilled class="w-6 h-6 hidden dark:block" />
+  <!-- <IconSunFilled class="w-6 h-6 dark:hidden" />
+  <IconMoonFilled class="w-6 h-6 hidden dark:block" /> -->
 </button>
