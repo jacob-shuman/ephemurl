70d5d48c5c1ce4fb87e87e1c7f66c28de85115ea
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-19 10:27:54 -0500

    fix: switch to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "hybrid",
-  adapter: vercel({ functionPerRoute: false }),
+  adapter: netlify()
 });
