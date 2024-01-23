542b70dec7dd7b4a91c9e0f10594fa18f5e59444
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-23 15:12:44 -0500

    feat: design overhaul

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,23 +1,87 @@
 <script lang="ts">
-  // import * as Icons from "@tabler/icons-svelte";
+  import tw from "clsx";
 
-  // export let icon: keyof typeof Icons;
-  export let url: string;
-
-  // const Icon: any = Icons[icon];
+  export let name: string;
+  export let inDevelopment: boolean;
+  export let url: string | undefined = undefined;
+  export let repo: string | undefined = undefined;
 </script>
 
-<a href={url} class="flex flex-col gap-y-2 group">
-  <!-- <div
-    class="bg-tinge dark:bg-bauhaus w-64 h-32 rounded-lg group-hover:-translate-y-2 scale transition-all duration-100 ease-out group-hover:bg-bauhaus group-hover:dark:bg-tinge flex justify-center items-center"
+<div
+  class={tw(
+    "flex flex-col gap-y-2 w-full sm:max-w-[16rem]",
+    !inDevelopment && "opacity-40"
+  )}
+>
+  <a
+    href={inDevelopment
+      ? url ?? (repo && `https://github.com/${repo}`) ?? undefined
+      : undefined}
+    class={tw(
+      "group focus:outline-none",
+      inDevelopment ? "cursor-pointer" : "cursor-not-allowed"
+    )}
+    on:mouseenter={() => {
+      if (inDevelopment) {
+        const audio = new Audio("audio/focus.ogg");
+        audio.volume = 0.25;
+        audio.play();
+      }
+    }}
+    on:focus={() => {
+      if (inDevelopment) {
+        const audio = new Audio("audio/focus.ogg");
+        audio.volume = 0.25;
+        audio.play();
+      }
+    }}
   >
-    <svelte:component
-      this={Icon}
-      class="w-12 h-12 text-grey group-hover:text-tinge group-hover:dark:text-bauhaus"
-    />
-  </div> -->
+    <div
+      class={tw(
+        "text-grey bg-tinge dark:bg-bauhaus w-full sm:w-64 h-32 rounded-lg flex justify-center items-center",
+        inDevelopment &&
+          "group-hover:-translate-y-2 group-focus:-translate-y-2 scale transition-all duration-100 ease-out group-hover:bg-bauhaus group-hover:dark:bg-tinge group-focus:bg-bauhaus group-focus:dark:bg-tinge"
+      )}
+    >
+      <slot name="icon" />
+    </div>
+  </a>
 
-  <div class="flex flex-col gap-y-1">
-    <slot />
+  <div class="flex flex-col gap-y-1 break-words">
+    <p class={tw("uppercase font-bold", !inDevelopment && "text-grey")}>
+      {name}
+    </p>
+
+    <div class="flex flex-col">
+      {#if url}
+        <a
+          href={inDevelopment ? url : undefined}
+          class={tw(
+            "flex gap-x-2 items-center group text-grey",
+            inDevelopment
+              ? "transition-colors duration-100 hover:text-bauhaus hover:dark:text-tinge"
+              : "cursor-not-allowed"
+          )}
+        >
+          <slot name="url-icon" />
+          <p class="line-clamp-1">{url}</p>
+        </a>
+      {/if}
+
+      {#if repo}
+        <a
+          href={inDevelopment ? `https://github.com/${repo}` : undefined}
+          class={tw(
+            "flex gap-x-2 items-center group text-grey",
+            inDevelopment
+              ? "transition-colors duration-100 hover:text-bauhaus hover:dark:text-tinge"
+              : "cursor-not-allowed"
+          )}
+        >
+          <slot name="vcs-icon" />
+          <p class="line-clamp-1">{repo}</p>
+        </a>
+      {/if}
+    </div>
   </div>
-</a>
+</div>
