5f1924b24f16764093d7caab4aeb6ae0f5f1407c
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-23 15:12:04 -0500

    feat: astro icon and clsx and tooltips

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
   output: "hybrid",
-  adapter: netlify(),
+  adapter: netlify()
 });
