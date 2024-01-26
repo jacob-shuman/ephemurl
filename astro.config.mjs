4a6f8f649729ca3098667f9ac9409f506e0e6447
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-26 15:22:05 -0500

    fix: use server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import netlify from "@astrojs/netlify";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), icon()],
-  output: "hybrid",
+  output: "server",
   adapter: netlify(),
 });
