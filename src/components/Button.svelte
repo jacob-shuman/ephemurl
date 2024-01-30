4700d85287a6d06a55b5fcc61e3f8ea705a1d911
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-01-30 15:09:46 -0500

    fix: dark mode button border

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -13,7 +13,7 @@
     class={tw(
       "cursor-pointer w-12 h-12 flex justify-center items-center rounded-lg text-grey",
       "transition-all duration-100 ease-in-out hover:bg-tinge hover:dark:bg-bauhaus",
-      "focus:outline-none border-2 border-transparent focus:border-black dark:border-white"
+      "focus:outline-none border-2 border-transparent focus:border-black dark:focus:border-white"
     )}
   >
     <slot />
