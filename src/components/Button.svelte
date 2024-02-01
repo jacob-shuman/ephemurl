0056a31dd796017697058325bc0f1c24fa4df6a9
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-02-01 17:51:41 -0500

    feat: button uses accent and focus colors

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -12,8 +12,8 @@
     {onclick}
     class={tw(
       "cursor-pointer w-12 h-12 flex justify-center items-center rounded-lg text-grey",
-      "transition-all duration-100 ease-in-out hover:bg-tinge hover:dark:bg-bauhaus",
-      "focus:outline-none border-2 border-transparent focus:border-black dark:focus:border-white"
+      "transition-all duration-100 ease-in-out hover:bg-accent hover:dark:bg-accent-dark",
+      "focus:outline-none border-2 border-transparent focus:border-focus dark:focus:border-focus-dark"
     )}
   >
     <slot />
