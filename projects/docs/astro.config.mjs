c28d6f798da8ba341ab35f67a5193192c39b282c
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-02-26 01:01:36 +0000

    feat: docs netlify plugin

--- projects/docs/astro.config.mjs
+++ projects/docs/astro.config.mjs
@@ -1,27 +1,29 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import netlify from "@astrojs/netlify";
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  adapter: netlify(),
+  integrations: [
+    starlight({
+      title: "EPHEMURL",
+      social: {
+        github: "https://github.com/jacob-shuman/ephemurl",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
