98b2d7fc94eada417fc49bd2f8822d0a0cf5e46d
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-03-12 16:06:08 -0400

    chore: remove netlify astro

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,14 +1,12 @@
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), icon()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
