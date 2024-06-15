5cc02fffebcbcd4136a86f844597fd2b086d2cdc
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-06-15 13:55:32 -0400

    feat: link button rename anchor classes

--- apps/site/src/components/LinkButton.svelte
+++ apps/site/src/components/LinkButton.svelte
@@ -4,25 +4,28 @@
   import Tooltip from "./Tooltip.svelte";
 
   export let tooltip: string;
+  export let active: boolean = false;
   export let href: string | undefined = undefined;
-  const anchorClass = tw(
+  const classes = tw(
     "cursor-pointer w-12 h-12 flex justify-center items-center rounded",
     "transition-all duration-100 ease-out",
     "hover:bg-fg-500 hover:dark:bg-fg-dark-500 hover:text-fg-100 hover:dark:text-fg-dark-100",
-    "focus:outline-none border-2 border-transparent focus:border-bg-100 dark:focus:border-bg-dark-100"
+    "focus:outline-none border-2 border-transparent focus:border-bg-100 dark:focus:border-bg-dark-100",
+
+    active && "bg-fg-500 dark:bg-fg-dark-500 text-fg-100 dark:text-fg-dark-100"
   );
 </script>
 
 {#if tooltip}
   <Tooltip message={tooltip}>
     <T.Trigger asChild let:builder>
-      <a use:builder.action {...builder} {href} class={anchorClass}>
+      <a use:builder.action {...builder} {href} class={classes}>
         <slot />
       </a>
     </T.Trigger>
   </Tooltip>
 {:else}
-  <a {href} class={anchorClass}>
+  <a {href} class={classes}>
     <slot />
   </a>
 {/if}
