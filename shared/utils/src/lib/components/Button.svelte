8d523a2fe8294d5e6815b68888a4c91dacb09096
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-06-29 14:44:24 -0400

    feat: button active bg color

--- shared/utils/src/lib/components/Button.svelte
+++ shared/utils/src/lib/components/Button.svelte
@@ -62,7 +62,7 @@
           "transition-all duration-100 ease-out",
           "hover:bg-fg-500 hover:dark:bg-fg-dark-500 hover:text-fg-100 hover:dark:text-fg-dark-100",
           "focus:outline-none ring-1 ring-transparent ring-offset-2 ring-offset-bg-500 dark:ring-offset-bg-dark-500 focus:ring-bg-100 focus:dark:ring-bg-dark-100",
-          "active:bg-bg-500 active:text-bg-100 active:dark:bg-bg-dark-500 active:dark:text-bg-dark-100",
+          "active:bg-bg-400 active:text-bg-100 active:dark:bg-bg-dark-400 active:dark:text-bg-dark-100",
           {
             "bg-fg-500 dark:bg-fg-dark-500 text-fg-100 dark:text-fg-dark-100":
               active,
